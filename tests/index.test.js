f8a71a33aef1fd7810fec2067a5fdff0098fc8eb
fletcher-greg <fletcher.web.dev.one@gmail.com>
2020-04-27 16:45:21 +0200

    add README

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,5 +5,5 @@ const add = (a, b) => a + b;
 
 const hi = () => "hi";
 
-test("should equal 2", () => assert.equal(3, add(1, 2)));
+test("should equal 2", () => assert.equal(3, add(1, 9)));
 test('should equal "hi"', () => assert.equal("hi", hi()));
